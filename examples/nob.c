c239c1fca5b33dfa2c687a9847f7e23ee9b3a423
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-04-26 18:49:55 +0200

    Rework build systems for examples

--- examples/nob.c
+++ examples/nob.c
@@ -8,12 +8,12 @@
     "-Wno-return-type", \
     "-Wno-discarded-qualifiers"
 
-#define RECIPE(dir, name)                                   \
+#define RECIPE(dir)                                         \
     do {                                                    \
         Nob_Cmd cmd = {0};                                  \
         nob_cmd_append(&cmd,                                \
             CC, CFLAGS, "-o",                               \
-            nob_temp_sprintf("./build/parsec_%s", name),    \
+            nob_temp_sprintf("./build/%s", dir),     \
             nob_temp_sprintf("./%s/main.c", dir)            \
         );                                                  \
         if (!nob_cmd_run(&cmd)) return 1;                   \
@@ -24,12 +24,12 @@ int main(int argc, char **argv) {
 
     if (!nob_mkdir_if_not_exists("build")) return 1;
 
-    RECIPE("00-hello-parsec",           "hello");
-    RECIPE("01-bools",                  "bools");
-    RECIPE("02-strings",                "strings");
-    RECIPE("03-lists",                  "lists");
-    RECIPE("10-password-generator",     "password");
-    RECIPE("11-csv",                    "csv");
+    RECIPE("00-hello-parsec");
+    RECIPE("01-bools");
+    RECIPE("02-strings");
+    RECIPE("03-lists");
+    RECIPE("10-password-generator");
+    RECIPE("11-csv");
 
     return 0;
 }
